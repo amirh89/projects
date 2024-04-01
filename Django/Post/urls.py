387d4bff1aac0ca6f12c792767892a7e6d949922
amir-h <165143348+amirh89@users.noreply.github.com>
2024-04-01 11:51:40 +0330

    Delete Django/Post/urls.py

--- Django/Post/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views
-
-app_name = 'blog'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/', views.PostListView.as_view(), name='post_list'),
-    path('posts/<pk>', views.post_detail, name='post_detail'),
-]
