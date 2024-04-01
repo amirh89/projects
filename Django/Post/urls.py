7cfef27b88debd732b034bcaaba2595aa814d805
amir-h <165143348+amirh89@users.noreply.github.com>
2024-04-01 12:05:15 +0330

    Create urls.py

---
+++ Django/Post/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('posts/<pk>', views.post_detail, name='post_detail'),
+]
