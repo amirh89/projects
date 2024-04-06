6f5c38677ee8a4cab8063539539c8039026ff33b
amir-h <165143348+amirh89@users.noreply.github.com>
2024-04-06 11:29:16 +0330

    Update urls.py

--- Django/Blog/urls.py
+++ Django/Blog/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.PostListView.as_view(), name='post_list'),
     path('posts/<pk>', views.post_detail, name='post_detail'),
+    path('posts/<post_id>/comment', views.post_comment, name='post_comment'),
+    path('ticket', views.ticket, name='ticket'),
+    path('postform', views.post_form, name='postform'),
+    path('login/', views.login_form, name='login'),
+    path('counter/', views.counter, name='counter'),
+    path('counter/counter', views.counter, name='counter'),
+    path('translation/', views.translate, name='translate'),
+    path('profile/', views.profile, name='profile'),
 ]
