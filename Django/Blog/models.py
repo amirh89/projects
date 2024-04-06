a0eb9b7c19ed471f4cf64c92292df28df73850dc
amir-h <165143348+amirh89@users.noreply.github.com>
2024-04-06 11:27:37 +0330

    Update models.py

--- Django/Blog/models.py
+++ Django/Blog/models.py
@@ -50,3 +50,80 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse("blog:post_detail", args=[self.id])
+    
+class Ticket(models.Model):
+    message = models.TextField(verbose_name='پیام')
+    name = models.CharField(max_length=250, verbose_name='نام')
+    email = models.EmailField(verbose_name='ایمیل')
+    phone = models.CharField(max_length=11, verbose_name='شماره تماس')
+    subject = models.CharField(max_length=250, verbose_name='موضوع')
+
+    class Meta:
+        verbose_name = 'تیکت'
+        verbose_name_plural = 'تیکت ها'
+
+
+
+    def __str__(self):
+        return self.name
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments", verbose_name='پست')
+    name = models.CharField(max_length=250, verbose_name='نام')
+    body = models.TextField(verbose_name='متن کامنت')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated = models.DateTimeField(auto_now=True, verbose_name='تاریخ ویرایش')
+    active = models.BooleanField(default=False, verbose_name='وضعیت')
+
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+        verbose_name = 'کامنت'
+        verbose_name_plural = 'کامنت ها'
+
+
+    def __str__(self):
+        return f'{self.name} : {self.post}'
+    
+
+class Login(models.Model):
+    password = models.CharField(max_length=200, verbose_name='پسوورد')
+    username = models.CharField(max_length=300 ,verbose_name='اسم کاربر')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated = models.DateTimeField(auto_now=True, verbose_name='تاریخ ویرایش')
+    active = models.BooleanField(default=False, verbose_name='فعالیت')
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+        verbose_name = 'لاگ این'
+        verbose_name_plural = 'لاگ این ها'
+
+    def __str__(self):
+        return f'{self.password}'
+    
+class Profile(models.Model):
+    full_name = models.CharField(max_length=200, verbose_name='نام و نام خانوادگی')
+    phone_number = models.IntegerField(verbose_name='شماره موبایل')
+    age = models.IntegerField(verbose_name='سن')
+    bio = models.CharField(max_length=300, verbose_name='بیو')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated = models.DateTimeField(auto_now=True, verbose_name='تاریخ ویرایش')
+    active = models.BooleanField(default=True, verbose_name='فعالیت')
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+        verbose_name='پروفایل'
+        verbose_name_plural='پروفایل ها'
+
+    def __str__(self):
+        return f'{self.full_name}'
