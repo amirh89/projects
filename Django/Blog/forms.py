c50ada9e4e509db3a22056e4c76610576e989cb8
amir-h <165143348+amirh89@users.noreply.github.com>
2024-04-13 15:12:15 +0330

    Update forms.py

--- Django/Blog/forms.py
+++ Django/Blog/forms.py
@@ -47,17 +47,10 @@ class CommentForm(forms.ModelForm):
                 'class':'cm_name'
             })
         }
-class PostForm(forms.Form):
-    VAHED_CHOICES=(
-        ('ثانیه','ثانیه'),
-        ('دقیقه','دقیقه'),
-        ('ساعت','ساعت'),
-    )
-    author = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'نویسنده', 'class':'auth'}))
-    title = forms.CharField(max_length=350, widget=forms.TextInput(attrs={'placeholder':'عنوان', 'style':'height:50px;'}))
-    description = forms.CharField(widget=forms.Textarea)
-    reading_time = forms.IntegerField(max_value=999)
-    vahed = forms.ChoiceField(choices=VAHED_CHOICES)
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['author','title','description','reading_time','vahed']
 
 class LoginForm(forms.Form):
     password = forms.IntegerField(max_value=999999, required=True, widget=forms.TextInput(attrs={'placeholder':'باید شش رقمی باشد','class':'psw'}))
