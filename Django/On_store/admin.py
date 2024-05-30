c9c41658ab82ee2abe607d1eafb93f8f059f8404
amir-h <165143348+amirh89@users.noreply.github.com>
2024-05-30 15:43:17 +0330

    Create admin.py

---
+++ Django/On_store/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','email','created','updated']
+    list_filter = ['first_name','last_name']
+    search_fields = ['phone','email']
+    list_editable = ['email']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','category']
+    list_filter = ['name','category']
+    search_fields = ['price']
+    list_editable = ['category']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['product','customer','price','quantity']
+    list_filter = ['price','status']
+    search_fields = ['price']
+    list_editable = ['quantity']
