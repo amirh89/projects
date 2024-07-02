02c37ee7678e1d298cd0d669c4d6ad3bce0fafe1
amir-h <165143348+amirh89@users.noreply.github.com>
2024-07-02 10:36:12 +0330

    Update models.py

--- Django/On_store/models.py
+++ Django/On_store/models.py
@@ -107,3 +107,13 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.name} : {self.product}'
+
+class Favorite(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'favorite'
+        verbose_name_plural = 'favorites'
+
+    def __str__(self):
+        return f"{self.product}"
