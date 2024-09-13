a8861e6fe652ce18d7814abd14ed93e17fc6c833
amir-h <165143348+amirh89@users.noreply.github.com>
2024-09-13 17:45:42 +0330

    Delete Django/Learning directory

--- Django/Learning/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-# Create your serializers here.
-
-class AddPackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LearningPackage
-        fields = ['name','description','author','category','image','video','price_with_discount']
