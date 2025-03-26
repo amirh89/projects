b60ddd5df823ad233ddea098d8f9a097dffae740
amir-h <165143348+amirh89@users.noreply.github.com>
2025-03-26 15:23:57 +0330

    Delete Django/portfolio directory

--- Django/portfolio/saving_media.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def generate_md5(file):
-    md5 = hashlib.md5()
-    for chunk in file.chunks():
-        md5.update(chunk)
-    return md5.hexdigest()
--- Django/portfolio/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-class Test(TestCase):
-    def test_status_code(self):
-        res = self.client.get('/hobby/objects/')
-        self.assertEqual(res.status_code, 200)
